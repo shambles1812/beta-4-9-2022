b02c5f8eb89b500386548cbf868a74649ebd0563
shambles1812 <cjvillafuerte02@gmail.com>
2022-04-09 18:20:02 +0800

    fix error at codePages.js

--- source-codes/codePage.js
+++ source-codes/codePage.js
@@ -4,17 +4,10 @@ var to_view_on_load;
 
 $.urlParam = function(name){
 	var results = new RegExp('[\?&]' + name + '=([^&#]*)').exec(window.location.href);
-<<<<<<< HEAD
 	if(results == null){
         return "none"
     }
     else{
-=======
-	if(results== null){
-        return "none";
-    }
-    else {
->>>>>>> beta
         return results[1];
     }
 }
@@ -30,71 +23,25 @@ $( document ).ready(function() {
         console.log("dataretrieved")
     });
     $(".codePreviewContainerText").css("zIndex","10");
-<<<<<<< HEAD
-=======
-    
->>>>>>> beta
 });
 $(".codeFlashCard").click(function() {
     var code_name = $(this).find("p").attr("value");
     console.log(code_name);
     $.getJSON('./data2.json', function(data) {
         html = data[code_name].html;
-        console.log(html);
-        $(".flipCardContainer.back").empty()
         
+        $(".flipCardContainer.back").empty()
         $(".flipCardContainer.back").append("<textarea class='sourceCodeArea'>"+html+"</textarea>")
-    });
+    })
     
-    if($(".cardGrid").data("flip-model").isFlipped){
-        $.getJSON('./data2.json', function(data) {
-            html = data[code_name].html;
-            $(".flipCardContainer.front").html(html);
-            $(".flipCardContainer.back").empty()
-            $(".flipCardContainer.back").append("<textarea class='sourceCodeArea'>"+html+"</textarea>")
-        })
+    $(".cardGrid")
+    .flip(true,function(){
         $(".flipCardContainer")[0].scrollIntoView(function(){});
         $(".cardGrid")
         .flip(false);
-        
+        $(".flipCardContainer.front").html(html);
         $(".codePreviewContainerText").css("zIndex","10");
-        if($("#codePrompt").hasClass("buttonToggle2")){
-            $(".flipCardContainer")[0].scrollIntoView(function(){});
-            $("#codePrompt").removeClass("buttonToggle2");
-            $("#codePrompt").empty();
-            $("#codePrompt").append("View Source Code");
-            $("#codePrompt").addClass("buttonToggle1");
-            
-        }
-        
-        
-}else{
-        $(".cardGrid")
-    .flip(true,function(){
-        $.getJSON('./data2.json', function(data) {
-            html = data[code_name].html;
-            $(".flipCardContainer.front").html(html);
-            $(".flipCardContainer.back").empty()
-            $(".flipCardContainer.back").append("<textarea class='sourceCodeArea'>"+html+"</textarea>")
-        })
-        $(".flipCardContainer")[0].scrollIntoView(function(){});
-        $(".cardGrid").flip(false);
-        
-        $(".codePreviewContainerText").css("zIndex","10");
-        
-        if($("#codePrompt").hasClass("buttonToggle2")){
-            $(".flipCardContainer")[0].scrollIntoView(function(){});
-            $("#codePrompt").removeClass("buttonToggle2");
-            $("#codePrompt").empty();
-            $("#codePrompt").append("View Source Code");
-            $("#codePrompt").addClass("buttonToggle1");
-            
-            
-        }
-        
     });
-    }
-    
     
 });
 
@@ -103,30 +50,17 @@ $(".codeFlashCard").click(function() {
   
 //     $(w.document.body).html("<h1>test</h1>");
 // })
-
+var current = true;
+var next = false;
+var buffer = true;
 $(".cardGrid").flip({
     trigger: "manual"
   });
 
 $("#codePrompt").click(function(){
-    if($("#codePrompt").hasClass("buttonToggle1") == true){
-        $(".cardGrid")
-        .flip(true);
-        $("#codePrompt").removeClass("buttonToggle1");
-        $("#codePrompt").addClass("buttonToggle2");
-        $("#codePrompt").empty();
-        $("#codePrompt").append("View Live Code");
-       
-    }else{
-        $(".cardGrid")
-        .flip(false);
-        $("#codePrompt").removeClass("buttonToggle2");
-        $("#codePrompt").addClass("buttonToggle1");
-        $("#codePrompt").empty();
-        $("#codePrompt").append("View Source Code");
-    }
-    
-    // current = next
-    // next = buffer;
-    // buffer = current;
+    $(".cardGrid")
+    .flip(current);
+    current = next
+    next = buffer;
+    buffer = current;
 })
