5ef9369a14a8dd0833b78e4769d93a59c4ea4826
shambles1812 <cjvillafuerte02@gmail.com>
2022-04-09 18:41:43 +0800

    codePage.js minor bug fix

--- source-codes/codePage.js
+++ source-codes/codePage.js
@@ -1,6 +1,6 @@
 var html;
 var to_view_on_load;
-
+var current_data;
 
 $.urlParam = function(name){
 	var results = new RegExp('[\?&]' + name + '=([^&#]*)').exec(window.location.href);
@@ -16,7 +16,8 @@ $( document ).ready(function() {
     console.log( "ready!" );
     to_view_on_load = $.urlParam('code_name');
     $.getJSON('./data2.json', function(data) {
-        
+        current_data =data;
+        console.log(current_data)
         if (to_view_on_load in data){
             $("#"+to_view_on_load).click();
         }
@@ -27,22 +28,16 @@ $( document ).ready(function() {
 });
 $(".codeFlashCard").click(function() {
     var code_name = $(this).find("p").attr("value");
-    console.log(code_name);
-    $.getJSON('./data2.json', function(data) {
-        html = data[code_name].html;
-        console.log(html);
-        $(".flipCardContainer.back").empty()
-        
-        $(".flipCardContainer.back").append("<textarea class='sourceCodeArea'>"+html+"</textarea>")
-    });
+    html = current_data[code_name].html
+ 
     
     if($(".cardGrid").data("flip-model").isFlipped){
-        $.getJSON('./data2.json', function(data) {
-            html = data[code_name].html;
-            $(".flipCardContainer.front").html(html);
-            $(".flipCardContainer.back").empty()
-            $(".flipCardContainer.back").append("<textarea class='sourceCodeArea'>"+html+"</textarea>")
-        })
+     
+        
+        $(".flipCardContainer.back").empty()
+        $(".flipCardContainer.back").append("<textarea class='sourceCodeArea'>"+html+"</textarea>")
+        $(".flipCardContainer.front").empty()
+        $(".flipCardContainer.front").append(html)
         $(".flipCardContainer")[0].scrollIntoView(function(){});
         $(".cardGrid")
         .flip(false);
@@ -61,12 +56,11 @@ $(".codeFlashCard").click(function() {
 }else{
         $(".cardGrid")
     .flip(true,function(){
-        $.getJSON('./data2.json', function(data) {
-            html = data[code_name].html;
-            $(".flipCardContainer.front").html(html);
-            $(".flipCardContainer.back").empty()
-            $(".flipCardContainer.back").append("<textarea class='sourceCodeArea'>"+html+"</textarea>")
-        })
+
+        $(".flipCardContainer.back").empty()
+        $(".flipCardContainer.back").append("<textarea class='sourceCodeArea'>"+html+"</textarea>")
+        $(".flipCardContainer.front").empty()
+        $(".flipCardContainer.front").append(html)
         $(".flipCardContainer")[0].scrollIntoView(function(){});
         $(".cardGrid").flip(false);
         
